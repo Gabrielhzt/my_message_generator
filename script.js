9590df971c03a92e22127ac69c78d7794ac70828
Gabrielhzt <hazout26@gmail.com>
2023-12-03 17:07:44 +0100

    Mise à jour du code avec de nouvelles phrases

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 const introductions = [
-    "Greetings!",
-    "Hello there!",
-    "Hi!",
-    "Hey!",
-    "Salutations!",
+    "Greetings and welcome!",
+    "Hello and good day!",
+    "Salutations and best wishes!",
+    "Greetings, fellow coder!",
+    "Hi there, coding enthusiast!",
 ];
 
 const bodies = [
-    "Innovation arises from perseverance and creativity.",
-    "Efficiency lies in the simplicity of code and clarity in documentation.",
-    "Collaboration among developers leads to more robust and versatile solutions.",
-    "Code modularity promotes reusability and simplifies long-term maintenance.",
-    "Constant curiosity fuels ongoing learning in the field of development.",
-    "Code optimization extends beyond execution speed; it also involves readability and maintainability.",
-    "Application scalability comes from thoughtful architectural choices and flexible design.",
-    "Clear documentation is as crucial as the code itself for ease of understanding and collaboration.",
-    "Proactive problem resolution ensures a seamless and reliable user experience.",
-    "Adaptability to new technologies is essential to remain relevant in an ever-changing technological landscape.",
+    "In the ever-evolving world of development, creativity sparks innovation.",
+    "Crafting efficient code requires a blend of simplicity and meticulous documentation.",
+    "Exploring the realms of collaboration leads to resilient and adaptable solutions.",
+    "Embracing code modularity fosters reusability, simplifying the path to long-term maintenance.",
+    "Curiosity is the driving force behind continuous learning in the dynamic field of development.",
+    "Optimizing code goes beyond execution speed; it entails enhancing readability and maintainability.",
+    "The scalability of an application is born from intentional architectural choices and flexible design.",
+    "In the world of coding, clear documentation is as vital as the code itself, facilitating understanding and collaboration.",
+    "Proactive problem resolution is the key to providing a seamless and reliable user experience.",
+    "Remaining relevant in the ever-changing tech landscape requires adaptability to new technologies.",
 ];
 
 const conclusions = [
-    "Keep coding!",
-    "Happy coding!",
-    "May your bugs be minimal and your code efficient!",
-    "Stay curious and keep learning!",
-    "Wishing you a productive coding session!",
+    "Keep coding passionately!",
+    "Wishing you a day filled with productive coding!",
+    "May your code be bug-free and your solutions efficient!",
+    "Stay curious, keep learning, and happy coding!",
+    "Here's to a coding session filled with accomplishment!",
 ];
 
 function getRandomComponent(components) {
@@ -41,4 +41,3 @@ function getRandomMessage() {
 }
 
 console.log(getRandomMessage());
-
